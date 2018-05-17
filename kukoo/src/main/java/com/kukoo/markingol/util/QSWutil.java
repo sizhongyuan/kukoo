1f8805a3b56f0e174e80d561c5da3e82a061b3fc
Bowengeli <34837183+Bowengeli@users.noreply.github.com>
2018-05-17 10:44:32 +0800

    又改回了魁北克省技术移民 (#76)

--- kukoo/src/main/java/com/kukoo/markingol/util/QSWutil.java
+++ kukoo/src/main/java/com/kukoo/markingol/util/QSWutil.java
@@ -26,7 +26,7 @@ public class QSWutil {
 	public static JSONObject getQSW(JSONArray question) throws ParseException{
 		int score = 0;//总得分
 		JSONObject reJson = new JSONObject();//返回json
-		reJson.put("projectName", "魁省技术移民");
+		reJson.put("projectName", "魁北克省技术移民");
 		reJson.put("country", "加拿大");
 		reJson.put("pass", 59);
 		reJson.put("intro", "该项目属于省提名移民项目；首先经魁省移民局审批获得省提名，再经联邦移民局进行健康及安全等审核。申请人须表明有意向居住在提名省份；获得永居身份后通常可迁徙至加拿大任意地区居住。该项目无职业限制，子女有额外加分，若申请人所学专业属于受训加分专业，则通常语言要求也较为宽松。但该项目需要抢名额，且处理周期相对长。");
